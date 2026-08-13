eb197e4b7cc3dac0b16c048632a47fabf47adb97
Libin <libinthankayathil@gmail.com>
2026-08-13 21:52:56 +0530

    add database schema

---
+++ backend/prisma/migrations/20260813161447_init/migration.sql
@@ -0,0 +1,33 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "email" TEXT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+-- CreateTable
+CREATE TABLE "Activity" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "userId" TEXT NOT NULL,
+    "sport" TEXT NOT NULL,
+    "unit" TEXT NOT NULL,
+    "value" REAL NOT NULL,
+    "points" INTEGER NOT NULL,
+    "recordedAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT "Activity_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_firstName_lastName_key" ON "User"("firstName", "lastName");
+
+-- CreateIndex
+CREATE INDEX "Activity_userId_idx" ON "Activity"("userId");
+
+-- CreateIndex
+CREATE INDEX "Activity_sport_idx" ON "Activity"("sport");
+
+-- CreateIndex
+CREATE INDEX "Activity_recordedAt_idx" ON "Activity"("recordedAt");
